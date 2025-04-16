59a986986f272b8ea5466023e7e7cbcf833143c8
Wong199 <alex.jimenez.wong@hotmail.com>
2025-04-15 19:28:55 -0600

    agragando secretos

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 import { handle } from 'hono/aws-lambda'
 
 const app = new Hono()
-
+console.log (process.env.DATABASE_NAME)
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
